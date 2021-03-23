667e85c189da52cb3d8e9839f8d49416c2bbfbde
Kenneth Hedman <github@vitberget.se>
2021-03-23 13:12:33 +0100

    Day 3 complete

---
+++ src/day3/mod.rs
@@ -0,0 +1,84 @@
+use std::collections::HashSet;
+use std::time::Instant;
+
+static PUZZLE: &'static str = include_str!("../../resources/puzzle3.txt");
+
+#[derive(PartialEq, Eq, Hash, Debug, Clone, Copy)]
+struct XY {
+    x: u32,
+    y: u32,
+}
+
+pub(crate) fn day3() {
+    println!("Day 3");
+
+    let trees = resource_to_trees();
+    let size = resource_to_size();
+
+    part1(&trees, &size);
+    part2(&trees, &size);
+}
+
+fn part1(trees: &HashSet<XY>, size: &XY) {
+    let i1 = Instant::now();
+    let p1 = calc_hits(&trees, &size, &XY { x: 3, y: 1 });
+    println!("Day 3, part1: {}, took {}micros", p1, i1.elapsed().as_micros());
+}
+
+fn part2(trees: &HashSet<XY>, size: &XY) {
+    let i2 = Instant::now();
+    let p2 = [
+        XY { x: 1, y: 1 },
+        XY { x: 3, y: 1 },
+        XY { x: 5, y: 1 },
+        XY { x: 7, y: 1 },
+        XY { x: 1, y: 2 }
+    ].iter()
+        .map(|step| calc_hits(&trees, &size, step))
+        .into_iter()
+        .fold(1, |a, v| a * v);
+    println!("Day 3, part2: {}, took {}micros", p2, i2.elapsed().as_micros())
+}
+
+fn calc_hits(trees: &HashSet<XY>, size: &XY, step: &XY) -> u32 {
+    let mut res: u32 = 0;
+    for y in (0..=size.y).step_by(step.y as usize) {
+        let current = XY { x: (step.x * y) % size.x, y: y };
+        if trees.contains(&current) {
+            res = res + 1;
+        }
+    }
+    return res;
+}
+
+fn resource_to_size() -> XY {
+    return XY {
+        x: PUZZLE.lines().next().unwrap().len() as u32,
+        y: PUZZLE.lines().count() as u32,
+    };
+}
+
+fn resource_to_trees() -> HashSet<XY> {
+    return PUZZLE.lines()
+        .enumerate()
+        .map(|(y, line)| line_to_trees(y as u32, line))
+        .flatten()
+        .collect();
+}
+
+fn line_to_trees(y: u32, line: &str) -> HashSet<XY> {
+    return line.chars()
+        .enumerate()
+        .map(|(x, c)| maybe_tree(x as u32, y, c))
+        .filter(|item| item.is_some())
+        .map(|item| item.unwrap())
+        .collect();
+}
+
+fn maybe_tree(x: u32, y: u32, c: char) -> Option<XY> {
+    return if c == '#' {
+        Some(XY { x, y })
+    } else {
+        None
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod day15;
+mod day3;
 
 fn main() {
     println!("Hello, world!");
 
-    println!("Day15 part1: {}",day15::day15(2020));
-    println!("Day15 part2: {}",day15::day15(30000000));
+    day3::day3()
+
+    // println!("Day15 part1: {}",day15::day15(2020));
+    // println!("Day15 part2: {}",day15::day15(30000000));
 }
